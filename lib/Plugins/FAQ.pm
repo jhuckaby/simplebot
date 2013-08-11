1de8ac97e9279725c978379304ccaed8a16b2273
Joseph Huckaby <jhuckaby@gmail.com>
2013-08-11 08:13:08 -0700

    Added FAQ 'list' and 'delete' sub-commands.

--- lib/Plugins/FAQ.pm
+++ lib/Plugins/FAQ.pm
@@ -8,6 +8,8 @@ package SimpleBot::Plugin::FAQ;
 # !faq NAME TEXT
 # !faq mychaninfo Visit our channel's website at http://mychannel.com/
 # !mychaninfo
+# !faq list
+# !faq delete 1
 ##
 
 use strict;
@@ -33,10 +35,12 @@ sub faq {
 	my $username = lc($args->{who});
 	my $chan = nch($args->{channel});
 	
+	if ($chan eq '#msg') { return "$username: The FAQ system only works in #channels."; }
+	
 	my $commands = $self->{bot}->{_eb_commands};
 	my $access = $self->{bot}->{_eb_data}->{plugin_access};
 	
-	if ($msg =~ /^(set\s*)?\!?(\w+)\s+(.+)$/) {
+	if ($msg =~ /^(set\s*)?\!?(\w+)\s+(.+)$/i) {
 		my ($faq, $text) = ($2, $3);
 		$faq = lc($faq);
 		
@@ -47,8 +51,8 @@ sub faq {
 		
 		# save faq name and text
 		$self->{data}->{channels} ||= {};
-		my $channel_data = $self->{data}->{channels}->{ sch($chan) } ||= {};
-		$channel_data->{$faq} = $text;
+		my $faqs = $self->{data}->{channels}->{ sch($chan) } ||= {};
+		$faqs->{$faq} = $text;
 		$self->dirty(1);
 		
 		# register command
@@ -57,6 +61,65 @@ sub faq {
 		
 		return "$username: FAQ command '$faq' saved for channel $chan.";
 	}
+	
+	elsif ($msg =~ /^list$/i) {
+		# list all faqs for this channel
+		$self->{data}->{channels} ||= {};
+		my $faqs = $self->{data}->{channels}->{ sch($chan) } ||= {};
+		
+		if (!scalar keys %$faqs) {
+			return "$username: There are no FAQ commands set for $chan.";
+		}
+		
+		my $response = '';
+		$response = "Here are the FAQ commands set for $chan:\n";
+		
+		my $idx = 0;
+		foreach my $faq (sort keys %$faqs) {
+			my $text = $faqs->{$faq};
+			$response .= "#" . int($idx + 1) . ": " . $faq . ": " . $text . "\n";
+			$idx++;
+		}
+		
+		$self->say(
+			channel => nch($chan),
+			body => $response
+		);
+		return undef;
+	}
+	
+	elsif ($msg =~ /^(delete|del|remove|rem)\s+(.+)$/i) {
+		# delete faq
+		my $which = $2;
+		
+		$self->{data}->{channels} ||= {};
+		my $faqs = $self->{data}->{channels}->{ sch($chan) } ||= {};
+		
+		if (!scalar keys %$faqs) {
+			return "$username: There are no FAQ commands set for $chan.";
+		}
+		
+		if ($which =~ /all/i) {
+			$self->{data}->{channels}->{ sch($chan) } = {};
+			$self->dirty(1);
+			return "$username: All FAQ commands deleted for $chan.";
+		}
+		if ($which !~ /^\d+$/) {
+			return "$username: Invalid syntax.  Please specify the FAQ number to delete.  Type !faq list to see a list of them.";
+		}
+		
+		my $faq_keys = [ sort keys %$faqs ];
+		my $faq_key = $faq_keys->[ int($which) - 1 ] || '';
+		if (!$faq_key) {
+			return "$username: FAQ index '$which' not found.  Type !faq list to see a list of them.";
+		}
+		
+		delete $faqs->{$faq_key};
+		$self->dirty(1);
+		
+		return "$username: FAQ command \#$which deleted.";
+	} # delete
+	
 	else {
 		return "$username: Incorrect syntax for !faq command.  See !help faq.";
 	}
