8bef382fca2bf56564a0269da9fcd72c5a52afeb
Joseph Huckaby <jhuckaby@gmail.com>
2014-04-12 12:31:30 -0700

    Now running all network commands in forks (ping, host, wget, telnet) Now handling DNS failures better (improved error message)

--- lib/Plugins/Net.pm
+++ lib/Plugins/Net.pm
@@ -8,6 +8,7 @@ package SimpleBot::Plugin::Net;
 # !ping HOST
 # !host HOST
 # !head URL
+# !telnet HOST PORT
 ##
 
 use strict;
@@ -29,16 +30,30 @@ sub ping {
 	my $username = lc($args->{who});
 	my $chan = nch($args->{channel});
 	
-	my $p = Net::Ping->new("external");
-	$p->hires();
-	my ($ret, $duration, $ip) = $p->ping($host, 1.0);
-	if ($ret) {
-		return sprintf("$host ($ip) is alive (time: %.2f ms)\n", 1000 * $duration);
-	}
-	else {
-		return "$host is down!  Ping failed yo!";
-	}
-	$p->close();
+	$self->{bot}->forkit(
+		channel => nch( $args->{channel} ),
+		who => $args->{who_disp},
+		run => sub {
+			eval {
+				my $p = Net::Ping->new("external");
+				$p->hires();
+				my ($ret, $duration, $ip) = $p->ping($host, 1.0);
+				if ($ret) {
+					print sprintf("$host ($ip) is online (ping time: %.2f ms)\n", 1000 * $duration) . "\n";
+				}
+				else {
+					print "Ping failed, $host is offline.\n";
+				}
+			}; # eval
+			if ($@) {
+				my $error_msg = $@;
+				$self->log_debug(2, "Ping Error: $error_msg");
+				print "$username: Ping Error: $error_msg\n";
+			}
+		} # sub
+	);
+	
+	return undef;
 }
 
 sub dns {
@@ -47,22 +62,37 @@ sub dns {
 	my $username = lc($args->{who});
 	my $chan = nch($args->{channel});
 	
-	my @ips = ();
-	if ($host =~ /^\d+\.\d+\.\d+\.\d+$/) {
-		# reverse ip-to-host
-		@ips = ( ipv4_to_hostname($host) );
-	}
-	else {
-		# host-to-ip
-		@ips = hostname_to_ipv4($host);
-	}
+	$self->{bot}->forkit(
+		channel => nch( $args->{channel} ),
+		who => $args->{who_disp},
+		run => sub {
+			eval {
+				my @ips = ();
+				if ($host =~ /^\d+\.\d+\.\d+\.\d+$/) {
+					# reverse ip-to-host
+					@ips = ( ipv4_to_hostname($host) );
+				}
+				else {
+					# host-to-ip
+					@ips = hostname_to_ipv4($host);
+				}
+				
+				if (@ips && ($ips[0] =~ /\S/)) {
+					print "$host resolved to: " . join(', ', @ips) . "\n";
+				}
+				else {
+					print "DNS ERROR: $host could not be resolved.\n";
+				}
+			}; # eval
+			if ($@) {
+				my $error_msg = $@;
+				$self->log_debug(2, "DNS Error: $error_msg");
+				print "$username: DNS Error: $error_msg\n";
+			}
+		} # sub
+	);
 	
-	if (@ips) {
-		return "$host resolved to: " . join(', ', @ips);
-	}
-	else {
-		return "ERROR: $host could not be resolved.";
-	}
+	return undef;
 }
 sub host { return dns(@_); }
 
@@ -72,16 +102,29 @@ sub head {
 	my $username = lc($args->{who});
 	my $chan = nch($args->{channel});
 	
-	my $ua = LWP::UserAgent->new();
-	$ua->timeout( 5 );
-	$ua->agent( "Mozilla/5.0 (SimpleBot 1.0 URL Tester)" );
-	
-	my $response = $ua->request( HTTP::Request->new( 'HEAD', $url ) );
-	
-	my $output = $response->as_string();
-	$output =~ s/\bClient\-[\w\-]+\:\s+[^\n]+//g; # strip useless "client" headers
-	
-	$self->say( %$args, body => $output );
+	$self->{bot}->forkit(
+		channel => nch( $args->{channel} ),
+		who => $args->{who_disp},
+		run => sub {
+			eval {
+				my $ua = LWP::UserAgent->new();
+				$ua->timeout( 5 );
+				$ua->agent( "Mozilla/5.0 (SimpleBot 1.0 URL Tester)" );
+				
+				my $response = $ua->request( HTTP::Request->new( 'HEAD', $url ) );
+				
+				my $output = $response->as_string();
+				$output =~ s/\bClient\-[\w\-]+\:\s+[^\n]+//g; # strip useless "client" headers
+				
+				print trim($output) . "\n";
+			}; # eval
+			if ($@) {
+				my $error_msg = $@;
+				$self->log_debug(2, "Network Error: $error_msg");
+				print "$username: Network Error: $error_msg\n";
+			}
+		} # sub
+	);
 	
 	return undef;
 }
@@ -96,32 +139,45 @@ sub tcp {
 	my $username = lc($args->{who});
 	my $chan = nch($args->{channel});
 	
-	if ($value =~ /^([\w\-\.]+)\D+(\d+)/) {
-		my ($host, $port) = ($1, $2);
-		my $socket = undef;
-		my $time_start = time();
-		
-		local $SIG{ALRM} = sub { die "ALRM\n" };
-		alarm 5;
-		eval {
-			$socket = new IO::Socket::INET(
-				PeerAddr => $host,
-				PeerPort => $port,
-				Proto => "tcp",
-				Type => SOCK_STREAM,
-				Timeout => 5
-			);
-		};
-		alarm 0;
-		if ($socket) {
-			my $elapsed = sprintf("%.2f ms", (time() - $time_start) * 1000);
-			return "TCP connection to $host:$port succeeded (time: $elapsed).";
-		}
-		else {
-			my $error_msg = $@ ? "Timeout" : $!;
-			return "ERROR: Could not open TCP socket to $host:$port: $error_msg";
-		}
-	}
+	$self->{bot}->forkit(
+		channel => nch( $args->{channel} ),
+		who => $args->{who_disp},
+		run => sub {
+			eval {
+				if ($value =~ /^([\w\-\.]+)\D+(\d+)/) {
+					my ($host, $port) = ($1, $2);
+					my $socket = undef;
+					my $time_start = time();
+					
+					local $SIG{ALRM} = sub { die "ALRM\n" };
+					alarm 5;
+					eval {
+						$socket = new IO::Socket::INET(
+							PeerAddr => $host,
+							PeerPort => $port,
+							Proto => "tcp",
+							Type => SOCK_STREAM,
+							Timeout => 5
+						);
+					};
+					alarm 0;
+					if ($socket) {
+						my $elapsed = sprintf("%.2f ms", (time() - $time_start) * 1000);
+						print "TCP connection to $host:$port succeeded (time: $elapsed).\n";
+					}
+					else {
+						my $error_msg = $@ ? "Timeout" : $!;
+						print "ERROR: Could not open TCP socket to $host:$port: $error_msg\n";
+					}
+				}
+			}; # eval
+			if ($@) {
+				my $error_msg = $@;
+				$self->log_debug(2, "Ping Error: $error_msg");
+				print "$username: Ping Error: $error_msg\n";
+			}
+		} # sub
+	);
 	
 	return undef;
 }
